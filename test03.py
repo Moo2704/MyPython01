9fcf6901208ceefdb8960ec49233ab61a250a521
Moo2704 <noimoo67@gmail.com>
2025-08-15 11:48:06 +0700

    V.2

---
+++ test03.py
@@ -0,0 +1,14 @@
+# คำสั่งแสดงผลข้อใดๆ ทางหน้าจอ print()
+#string
+print('hello')
+print("hi")
+print('''hey''')
+print("""hmm""")
+
+#Number
+print(1234) #integer/int
+print(3.14124) # float
+
+# boolen ตรรกะ มีแค่ true(จริง) และ False(เท็จ)
+print(True)
+print(False)
